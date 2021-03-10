acb9206c87bedb80fd2c6cc2870eed374184cdcd
Maks <maksymmaksymeniuk@gmail.com>
2021-03-10 18:51:43 +0200

    update

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -16,25 +16,25 @@ export class NavbarComponent implements OnInit {
   public navigations = [
     {
       title: 'Infrastructure',
-      image: '/assets/icon/navbar/infastructure/Infrastructure.svg',
+      image: './assets/icon/navbar/infastructure/Infrastructure.svg',
       path: 'infrastructure',
       color: '#FEAE1B',
       children: [
         {
           title: 'Services',
-          image: '/assets/icon/navbar/infastructure/sub/Services.svg',
+          image: './assets/icon/navbar/infastructure/sub/Services.svg',
           path: 'services',
           color: '',
           children: []
         }, {
           title: 'Ports',
-          image: '/assets/icon/navbar/infastructure/sub/Ports.svg',
+          image: './assets/icon/navbar/infastructure/sub/Ports.svg',
           path: 'ports',
           color: '',
           children: []
         }, {
           title: 'Cluster',
-          image: '/assets/icon/navbar/infastructure/sub/Cluster.svg',
+          image: './assets/icon/navbar/infastructure/sub/Cluster.svg',
           path: 'cluster',
           color: '',
           children: []
@@ -42,96 +42,96 @@ export class NavbarComponent implements OnInit {
       ]
     }, {
       title: 'Rules',
-      image: '/assets/icon/navbar/rules/Rules.svg',
+      image: './assets/icon/navbar/rules/Rules.svg',
       path: 'rulse',
       color: '#4339F2',
       children: []
     }, {
       title: 'Authetication',
-      image: '/assets/icon/navbar/authetication/Authetication.svg',
+      image: './assets/icon/navbar/authetication/Authetication.svg',
       path: 'authetication',
       color: '#37C5AB',
       children: [{
         title: 'Policies',
-        image: '/assets/icon/navbar/authetication/sub/Policies.svg',
+        image: './assets/icon/navbar/authetication/sub/Policies.svg',
         path: 'policies',
         color: '',
         children: []
       }, {
         title: 'Connectors',
-        image: '/assets/icon/navbar/authetication/sub/Connectors.svg',
+        image: './assets/icon/navbar/authetication/sub/Connectors.svg',
         path: 'connectors',
         color: '',
         children: []
       }, {
         title: 'Authetication steps',
-        image: '/assets/icon/navbar/authetication/sub/AutheticationSteps.svg',
+        image: './assets/icon/navbar/authetication/sub/AutheticationSteps.svg',
         path: 'authetication-steps',
         color: '',
         children: []
       },]
     }, {
       title: 'Trusted groups',
-      image: '/assets/icon/navbar/trusted-groups/TrustedGroups.svg',
+      image: './assets/icon/navbar/trusted-groups/TrustedGroups.svg',
       path: 'trusted-groups',
       color: '#FF551B',
       children: []
     }, {
       title: 'Reports',
-      image: '/assets/icon/navbar/reports/Reports.svg',
+      image: './assets/icon/navbar/reports/Reports.svg',
       path: 'reports',
       color: '#7A6FF0',
       children: [{
         title: 'Audit',
-        image: '/assets/icon/navbar/reports/sub/Audit.svg',
+        image: './assets/icon/navbar/reports/sub/Audit.svg',
         path: 'audit',
         color: '',
         children: []
       }, {
         title: 'User activity',
-        image: '/assets/icon/navbar/reports/sub/UserActivity.svg',
+        image: './assets/icon/navbar/reports/sub/UserActivity.svg',
         path: 'user-activity',
         color: '',
         children: []
       }, {
         title: 'Logs',
-        image: '/assets/icon/navbar/reports/sub/Logs.svg',
+        image: './assets/icon/navbar/reports/sub/Logs.svg',
         path: 'logs',
         color: '',
         children: []
       },]
     }, {
       title: 'Settings',
-      image: '/assets/icon/navbar/settings/Settings.svg',
+      image: './assets/icon/navbar/settings/Settings.svg',
       path: 'settings',
       color: '#62D26F',
       children: [{
         title: 'General',
-        image: '/assets/icon/navbar/settings/sub/General.svg',
+        image: './assets/icon/navbar/settings/sub/General.svg',
         path: 'general',
         color: '',
         children: []
       }, {
         title: 'License',
-        image: '/assets/icon/navbar/settings/sub/License.svg',
+        image: './assets/icon/navbar/settings/sub/License.svg',
         path: 'license',
         color: '',
         children: []
       }, {
         title: 'Web SSL',
-        image: '/assets/icon/navbar/settings/sub/WebSSL.svg',
+        image: './assets/icon/navbar/settings/sub/WebSSL.svg',
         path: 'web',
         color: '',
         children: []
       }, {
         title: 'User managment',
-        image: '/assets/icon/navbar/settings/sub/UserManagment.svg',
+        image: './assets/icon/navbar/settings/sub/UserManagment.svg',
         path: 'user-managment',
         color: '',
         children: []
       }, {
         title: 'EULA',
-        image: '/assets/icon/navbar/settings/sub/EULA.svg',
+        image: './assets/icon/navbar/settings/sub/EULA.svg',
         path: 'eula',
         color: '',
         children: []
